d04e296ae1a1a79814328692e924338c5f2d757e
laladu29 <music29@hotmail.fr>
2016-04-25 18:24:45 +0000

    Mise en place du <canvas> et commencement de l'écriture du script 'JSGraph.js'

---
+++ client/JSGraph.js
@@ -0,0 +1,11 @@
+function begin() {
+    $('canvas').drawLine({
+        strokeStyle: "black",
+        strokeWidth: 4,
+        x1:0, y1:0,
+        x2:0, y2:300,
+        x3:400, y3:300,
+    });
+}
+
+begin();
