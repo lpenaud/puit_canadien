0d3bcb57bf02126973080be0f716392385c0ac00
laladu29 <music29@hotmail.fr>
2016-05-04 09:51:42 +0000

    Amélioration du design

--- server.js
+++ server.js
@@ -7,22 +7,34 @@ var app = express();
 var server = http.createServer(app);
 var io = socketio.listen(server);
 var jsonfile = require('jsonfile');
-var util = require('util');
 var delayed = require("delayed");
 
-var file0 = './sample.json', file1='./sampleFutur.json';
+var file=['sample.json','sampleFutur.json','curve.json'];
 
 function present(array) {
-    var present=[jsonfile.readFileSync(file0).tWell,
-        jsonfile.readFileSync(file0).tHumi,
-        jsonfile.readFileSync(file0).tVMC];
+    var present=[jsonfile.readFileSync(file[0]).tWell,
+        jsonfile.readFileSync(file[0]).tHumi,
+        jsonfile.readFileSync(file[0]).tVMC];
         
-    if (array==null) {
+    if (!array) {
         return present
     }
     else {
         return present[array]
     }
+}
+
+function dataCurve(time) {
+    var data=jsonfile.readFileSync(file[2]).data,dataFutur=[];
+    console.log("Longueur boucle : "+data.length);
+    //console.log("dataRead : "+data[1].date+'/'+data[1].mouth+'/'+data[1].year);
+    
+    for(var i=data.length-1;i>-1;i--) {
+        if (time.year==data[i].year && time.mouth==data[i].mouth && time.date==data[i].date) {
+            dataFutur.push(data[i]);
+        }
+    }
+    return dataFutur
 }
 
     io.sockets.on('connection', function (socket) {
@@ -31,10 +43,16 @@ function present(array) {
     socket.on('input', function (input) {
         console.log("input : "+input);
         var inputF={tWell: input[0],tHumi: present(1), tVMC: input[1]};
-        jsonfile.writeFile(file1,inputF, function (err) {
-            console.error("Erreur de l'écriture de : "+file1+' : '+err);
+        jsonfile.writeFile(file[1],inputF, function (err) {
+            console.error("Erreur de l'écriture de : "+file[1]+' : '+err);
             socket.emit('err',err);
             });
+    });
+    
+    socket.on('time', function(time) {
+       console.log(time.date+'/'+time.mouth+'/'+time.year);
+       console.log(dataCurve(time));
+       socket.emit('dataCurve',dataCurve(time));
     });
 });
 
@@ -44,8 +62,5 @@ server.listen(process.env.PORT || 80, process.env.IP || '127.0.0.1', function ()
     var addr = server.address();
     console.log("Server listening at", addr.address + ":" + addr.port);
 });
-
-
-
 
 
